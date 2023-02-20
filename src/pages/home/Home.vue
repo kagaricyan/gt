fbe824146b74a9720b16f264e13063538cd7ecaf
kagari <2285610925@qq.com>
2023-02-20 19:47:53 +0800

    样式调整

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="top">
+  <div class="home">
+    <img class="fixed-top-left" src="../../assets/images/20230106.png" alt="会战logo">
     <div class="chart" ref="chartRef"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue';
-import attackRecord from '../../assets/31-attack-record.json';
+import attackRecord from '../../assets/attact-record.json';
 import { AttackRecord } from '../../data';
 import * as echarts from 'echarts';
 
@@ -15,24 +16,36 @@ type LogDate = string;
 type BossName = string;
 type Damage = number;
 
+const elementColors: Record<string, string> = {
+  光: '#ffd71c',
+  虚: '#5e778b',
+  暗: '#a50adf',
+  土: '#9a7b2d',
+  水: '#2287c1',
+  火: '#c24913',
+};
+
 const chartRef = ref<HTMLElement>();
 const bossNameCorrect: Record<string, string> = {
   boss_nine_tailed_fox_guild: '水狐',
   水狐: 'boss_nine_tailed_fox_guild',
+  boss_invader_director_guild: '导演',
+  导演: 'boss_invader_director_guild',
 };
 const option: echarts.EChartsOption = {
-  // tooltip: {
-  //   trigger: 'axis',
-  //   axisPointer: {
-  //     // Use axis to trigger tooltip
-  //     type: 'shadow', // 'shadow' as default; can also be 'line' or 'shadow'
-  //   },
-  // },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      // Use axis to trigger tooltip
+      type: 'shadow', // 'shadow' as default; can also be 'line' or 'shadow'
+    },
+  },
   legend: {
     top: '40',
+    right: '80',
   },
   grid: {
-    top: '80',
+    top: '100',
     left: '5%',
     right: '5%',
     bottom: '3%',
@@ -48,68 +61,7 @@ const option: echarts.EChartsOption = {
     type: 'category',
     data: [],
   },
-  series: [
-    {
-      name: 'Direct',
-      type: 'bar',
-      stack: 'total',
-      label: {
-        show: true,
-      },
-      emphasis: {
-        focus: 'series',
-      },
-      data: [320, 302, 301, 334, 390, 330, 320],
-    },
-    {
-      name: 'Mail Ad',
-      type: 'bar',
-      stack: 'total',
-      label: {
-        show: true,
-      },
-      emphasis: {
-        focus: 'series',
-      },
-      data: [120, 132, 101, 134, 90, 230, 210],
-    },
-    {
-      name: 'Affiliate Ad',
-      type: 'bar',
-      stack: 'total',
-      label: {
-        show: true,
-      },
-      emphasis: {
-        focus: 'series',
-      },
-      data: [220, 182, 191, 234, 290, 330, 310],
-    },
-    {
-      name: 'Video Ad',
-      type: 'bar',
-      stack: 'total',
-      label: {
-        show: true,
-      },
-      // emphasis: {
-      //   focus: 'series'
-      // },
-      data: [150, 212, 201, 154, 190, 330, 410],
-    },
-    {
-      name: 'Search Engine',
-      type: 'bar',
-      stack: 'total',
-      label: {
-        show: true,
-      },
-      emphasis: {
-        focus: 'series',
-      },
-      data: [820, 832, 901, 934, 1290, 1330, 1320],
-    },
-  ],
+  series: [],
 };
 const buildOption = () => {
   let bosses: string[] = [];
@@ -159,6 +111,7 @@ const buildOption = () => {
   option.yAxis.data = usersSortByDamage;
   option.series = bosses.map(boss => {
     const originalBossName = bossNameCorrect[boss.split('--')[0]] ?? boss.split('--')[0];
+    const bossElement = boss.split('--')[1];
     return {
       name: boss,
       type: 'bar',
@@ -170,6 +123,7 @@ const buildOption = () => {
           return String(res.value).replace(/(\d)(?=(\d{3})+$)/g, '$1,');
         },
       },
+      color: `${ elementColors[bossElement] }d0`,
       data: usersSortByDamage.map(user => userBossData[user][originalBossName] ?? 0),
     };
   });
@@ -199,8 +153,13 @@ onMounted(() => {
 </script>
 
 <style scoped>
+.fixed-top-left {
+  height: 60px;
+  margin: 40px 0 -80px 40px;
+}
+
 .chart {
   height: 2200px;
-  margin-bottom: 1000px;
+  padding-bottom: 60px;
 }
 </style>
