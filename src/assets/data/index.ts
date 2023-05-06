a72a16862f24a58c564e6dd933945b9b712d685e
kagari <2285610925@qq.com>
2023-05-06 17:04:09 +0800

    更新数据

--- src/assets/data/index.ts
+++ src/assets/data/index.ts
@@ -2,10 +2,12 @@ import a from './a.json';
 import b from './b.json';
 import c from './c.json';
 import d from './d.json';
+import e from './e.json';
 
 export default [
   { name: '绚烂泳池派对', data: a },
   { name: '法老墓穴', data: b },
   { name: '课间屋顶', data: c },
   { name: '风驰电掣', data: d },
+  { name: '魔法禁书库', data: e },
 ];
